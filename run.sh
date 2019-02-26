38919346bd8b9baabde653abe978dad9dc8a44a1
Chengkai Zhu <cpsai@gra-login3.graham.sharcnet>
2019-02-25 20:49:31 -0500

    script modified

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #SBATCH --account=def-cpsmcgil
-#SBATCH --gres=gpu:1              # Number of GPUs (per node)
+#SBATCH --gres=gpu:2              # Number of GPUs (per node)
 #SBATCH --mem=12000M               # memory (per node)
-#SBATCH --time=0-03:00            # time (DD-HH:MM)
+#SBATCH --time=00:12:00
 module purge
 module load  python/3.6.3
 source ~/ENV/bin/activate
